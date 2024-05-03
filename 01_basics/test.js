9564861b27c74e8fed3d29c217aa3a5d3e75cd8a
Aditya Bahuguna <146475116+adityabahuguna9548@users.noreply.github.com>
2024-05-03 18:48:02 +0000

    test variables in js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,22 @@
 console.log("Hey my name is Aditya Bahuguna");
 console.log("This is test");
+
+const accountId = 123456;
+let accounEmail = "aditya@gmail.com";
+var accountPassword = "12345";
+let accountState;
+
+accountCity = "Rishikesh";
+
+// accountId = 234567 // not allowed
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+accounEmail = "adityabahuguna@gmail.com";
+accountPassword = "123456789";
+
+console.log(accountId);
+console.table([accountId,accounEmail,accountPassword,accountCity,accountState]);
