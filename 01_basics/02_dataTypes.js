4d354318c6001f76a0566d9c7b054bbca91f1ab5
Aditya Bahuguna <146475116+adityabahuguna9548@users.noreply.github.com>
2024-05-04 11:33:52 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+
+let name = "Aditya Bahuguna"; // string
+let age = 23; // number
+let isLoggedIn = false; // boolean
+let state ; // undefined
+let pp = null; // null 
+
+/*
+types of datatype-
+null
+number 
+string
+symbol
+boolean
+BigInt
+undefined
+*/
+console.table([typeof name,age,isLoggedIn,state,pp]);
