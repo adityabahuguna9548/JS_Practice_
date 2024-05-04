0daee4a55e81fff1e4079a577b1d2b256cf29751
Aditya Bahuguna <146475116+adityabahuguna9548@users.noreply.github.com>
2024-05-04 11:51:44 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33";
+console.log(typeof score);
+
+let isInNumber = Number(score);
+console.log(typeof isInNumber);
+
+
+// after conversion
+//"33" => 33
+//"" => NaN - Not a Number 
+
+let checkBoolean = null;
+// console.log(typeof checkBoolean);
+
+let isInBoolean = Boolean(checkBoolean);
+console.log( isInBoolean);
+
+// 0 => false
+// 1 => true
+// "" =>false
+// "aditya " => true
+// null => false
+
+let someNumber = 33;
+let stringNumber =  String(someNumber);
+console.log(stringNumber,typeof stringNumber);
+
