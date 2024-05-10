620e2e541a40a814dd168a718a2ca946474f41dd
Aditya Bahuguna <146475116+adityabahuguna9548@users.noreply.github.com>
2024-05-10 18:49:23 +0000

    stack & heap memory example

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -13,11 +13,11 @@ console.log(id === anotherId);
 
 const str = "aditya" // string
 
-const numBigint 78632864768326n // BigInt
+const numBigint = 78632864768326n // BigInt
 
 const checkValue = true ; // boolean another one is false
 
-const emptyVar ; // Undefined 
+let emtVar ; // Undefined 
 
 
 //Reference(Non Primitive ) type :- 3 types
@@ -35,3 +35,34 @@ const myObj = {
 const newFn =   function(){
     console.log("Hello World!");
 };
+
+
+
+//***************Memory in Js **************/
+
+// stack ( Primitive data types)  & Heap (Non Primitive  data types)
+
+//stack :- variable copied in memory 
+
+let currentName = "Aditya Bahuguna";
+
+let newName = currentName;
+
+newName = "Shubham Bahuguna";
+
+console.log(currentName);
+console.log(newName);
+
+// Heap :- called reference of object value 
+let userOne = {
+    name : "Aditya Bahuguna",
+    class : "MCA",
+    email : "adityabahuguna48@gmail.com"
+};
+
+let userTwo = userOne;
+
+userTwo.email = "adityabahugun12345@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
