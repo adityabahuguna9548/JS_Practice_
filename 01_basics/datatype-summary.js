cd5c1b118d30d3ccaddb1f6654cca2a4ae870feb
Aditya Bahuguna <146475116+adityabahuguna9548@users.noreply.github.com>
2024-05-10 18:12:30 +0000

    datatype summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,37 @@
+// Primitive data types :-
+
+// 7 types data types 
+
+// null , Number , Symbol , String , BigInt , Boolean , Undefined
+
+const chekNull = null ; // null
+const idNum = 123  // number
+
+const id = Symbol('123') // symbol
+const anotherId = Symbol('123') //
+console.log(id === anotherId);
+
+const str = "aditya" // string
+
+const numBigint 78632864768326n // BigInt
+
+const checkValue = true ; // boolean another one is false
+
+const emptyVar ; // Undefined 
+
+
+//Reference(Non Primitive ) type :- 3 types
+
+// Arrays , Functions , Objects
+
+const heros = ["Shaktiman" , "naagraj", "doga"] ; // Array
+
+const myObj = {
+    name : "Aditya",
+    class : "mca",
+    id : 2205002
+};  // Objects
+
+const newFn =   function(){
+    console.log("Hello World!");
+};
