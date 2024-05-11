2dbf3b5571211ce02c50f7637733f4d849e9cbc9
Aditya Bahuguna <146475116+adityabahuguna9548@users.noreply.github.com>
2024-05-12 00:06:34 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -13,12 +13,12 @@ console.log(gameName[0]); // position on string
 console.log(gameName.__proto__);// check protoype it is object of string
 console.log(gameName.toLowerCase());
 
-console.log(gameName.charAt(4));
-console.log(gameName.indexOf('d'));
+console.log(gameName.charAt(4)); // check charactor which index is in stored
+console.log(gameName.indexOf('d')); // check charator index 
 
-console.log(gameName.substring(0,5));
-console.log(gameName.slice(0,3));
-console.log(anotherGameName.trim());
-console.log(anotherGameName.replace('-',' dib '));
-console.log(anotherGameName.includes('aadi'));
-console.log(anotherGameName.split('-'));
+console.log(gameName.substring(0,5)); // convert into small string
+console.log(gameName.slice(0,3)); // devided into slice form
+console.log(anotherGameName.trim()); // remove spaces on strings
+console.log(anotherGameName.replace('-',' dib ')); // replace charator to another charator
+console.log(anotherGameName.includes('aadi')); // find charactor in strings
+console.log(anotherGameName.split('-'));// convert string into array form
